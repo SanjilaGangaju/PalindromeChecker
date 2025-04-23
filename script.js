f97cdb27454f5aba1948084c7b2c56ea8dc2609b
Sanjila Gangaju <canzilagangaju@gmail.com>
2025-04-23 22:32:12 +0545

    checks palindrome of the input

--- script.js
+++ script.js
@@ -3,5 +3,17 @@ let inputString=document.getElementById("text-input");
 let checkButton = document.getElementById("check-button");
 checkButton.addEventListener("click",() =>{
     let inputValue = inputString.value;
-    console.log(inputValue);
+    let inputArray=inputValue.split("");
+    let reverseString= inputArray.reverse().join("");
+    if (reverseString===inputValue){
+        resultDisplay.style.color="green";
+        resultDisplay.innerText=`Hola! ${inputValue} is a Palindrome.`;
+
+    }
+    else{
+        resultDisplay.style.color="red";
+        resultDisplay.innerText= `Oops! ${inputValue} is not a Palindrome.`;
+        
+    }
+
 })
