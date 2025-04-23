aae459ece86f93847802256048ff3b4a059ac383
Sanjila Gangaju <canzilagangaju@gmail.com>
2025-04-23 22:16:56 +0545

    dynamic input access

---
+++ script.js
@@ -0,0 +1,7 @@
+let resultDisplay=document.querySelector(".result");
+let inputString=document.getElementById("text-input");
+let checkButton = document.getElementById("check-button");
+checkButton.addEventListener("click",() =>{
+    let inputValue = inputString.value;
+    console.log(inputValue);
+})
